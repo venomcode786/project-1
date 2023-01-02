8501cced9612d9d9a423d816d54e5d8bacf5a40b
venomcode786 <121813745+venomcode786@users.noreply.github.com>
2023-01-02 20:50:24 +0530

    Add files via upload

---
+++ linkedlist.cpp
@@ -0,0 +1,27 @@
+#include<math.h>
+#include<iostream>
+#include<utility>
+
+
+using namespace std;
+
+
+class Node{
+    public:
+    int data;
+    Node*next;
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+
+int main(){
+    Node*node1 = new Node(10);
+
+    cout<<node1->data<<" ";
+    cout<<node1->next<<" ";
+}
